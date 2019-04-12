9dcdf15d8338aa3884e2ed8ab3895d144cc14706
Dominic <domburns97@outlook.com>
2019-04-12 17:52:21 +0100

    Window

--- Engine/Mesh.cpp
+++ Engine/Mesh.cpp
@@ -23,14 +23,55 @@ Mesh * Mesh::genCube()
 	m->verts = new vec3f[m->numVerts];
 	m->inds = new int[m->numInds];
 
-	//m->verts[0] = (-1.f,  1.f,  1.f);
-	//m->verts[1] = ( 1.f,  1.f,  1.f);
-	//m->verts[2] = (-1.f, -1.f,  1.f);
-	//m->verts[3] = ( 1.f, -1.f,  1.f);
-	//m->verts[4] = ( 1.f,  1.f, -1.f);
-	//m->verts[5] = (-1.f,  1.f, -1.f);
-	//m->verts[6] = ( 1.f, -1.f, -1.f);
-	//m->verts[7] = (-1.f, -1.f, -1.f);
+	m->verts[0] = {-1.f,  1.f,  1.f};
+	m->verts[1] = { 1.f,  1.f,  1.f};
+	m->verts[2] = {-1.f, -1.f,  1.f};
+	m->verts[3] = { 1.f, -1.f,  1.f};
+	m->verts[4] = { 1.f,  1.f, -1.f};
+	m->verts[5] = {-1.f,  1.f, -1.f};
+	m->verts[6] = { 1.f, -1.f, -1.f};
+	m->verts[7] = {-1.f, -1.f, -1.f};
+
+	m->inds[0 ] = 1;
+	m->inds[1 ] = 0;
+	m->inds[2 ] = 2;
+	m->inds[3 ] = 3;
+	m->inds[4 ] = 1;
+	m->inds[5 ] = 2;
+
+	m->inds[6 ] = 0;
+	m->inds[7 ] = 0;
+	m->inds[8 ] = 0;
+	m->inds[9 ] = 0;
+	m->inds[10] = 0;
+	m->inds[11] = 0;
+	m->inds[12] = 0;
+	m->inds[13] = 0;
+	m->inds[14] = 0;
+	m->inds[15] = 0;
+	m->inds[16] = 0;
+	m->inds[17] = 0;
+	m->inds[18] = 0;
+	m->inds[19] = 0;
+	m->inds[20] = 0;
+	m->inds[21] = 0;
+	m->inds[22] = 0;
+	m->inds[23] = 0;
+	m->inds[24] = 0;
+	m->inds[25] = 0;
+	m->inds[26] = 0;
+	m->inds[27] = 0;
+	m->inds[28] = 0;
+	m->inds[29] = 0;
+	m->inds[30] = 0;
+	m->inds[31] = 0;
+	m->inds[32] = 0;
+	m->inds[33] = 0;
+	m->inds[34] = 0;
+	m->inds[35] = 0;
+
+
+	
 
 	return m;
 }
--- Engine/Source.cpp
+++ Engine/Source.cpp
@@ -3,7 +3,6 @@
 
 int main() {
 
-	vec3f v = { 2,3,2 };
-	std::cout << v[0] << " " << v[1] << " " << v[2];
+
 	getchar();
 }
